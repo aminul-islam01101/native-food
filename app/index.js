c5024021504c3350da7f655e9493157eee2044c4
aminul-islam01101 <aminul.islam01101@gmail.com>
2024-02-28 14:52:37 +0600

    ADDED: project starts.

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+const index = () => {
+  return (
+  //  <Redirect href="/(authenticate)/login"/>
+  <View>
+    <Text>Indexes</Text>
+  </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
+

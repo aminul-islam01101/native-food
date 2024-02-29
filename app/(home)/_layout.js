f272d0c980c63d95a641a496af246bce7dceeaa9
aminul-islam01101 <aminul.islam01101@gmail.com>
2024-02-29 11:18:28 +0600

    ADDED: redux and redux toolkit .

--- app/(home)/_layout.js
+++ app/(home)/_layout.js
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import store from "../../store";
 
 
 
 export default function Layout() {
   return (
-
+    <Provider store={store}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="hotel" />
   
       </Stack>
+    </Provider>
    
   );
 }
